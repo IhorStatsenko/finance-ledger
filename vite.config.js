63bfba749dd868dc5a70b4d8406cf98f4dbee6ab
IhorStatsenko <ihorstatsenko@gmail.com>
2025-01-31 23:08:21 +0200

    folder configuration and some basic scss tests

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,18 @@
 import { defineConfig } from 'vite';
+// import { resolve } from 'path';
 
 export default defineConfig({
   base: '/finance-ledger/',
+  // resolve: {
+  //   alias: {
+  //     '@': resolve(__dirname, 'src'),
+  //   },
+  // },
+  // css: {
+  //   preprocessorOptions: {
+  //     scss: {
+  //       additionalData: `@use "@/styles/utils/variables.scss";`,
+  //     },
+  //   },
+  // },
 });
